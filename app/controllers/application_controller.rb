f615a41ec09ba50f97f4339430a6663417d08982
jonah sobol <jonah.sobol@gmail.com>
2016-02-25 17:21:33 -0800

    fix user id potential bug

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,7 +9,7 @@ class ApplicationController < ActionController::Base
 
     def current_user
       # reset_session
-      @current_user ||= User.find(session[:user_id]) if session[:user_id]
+      @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
     end
 
     def authorize
