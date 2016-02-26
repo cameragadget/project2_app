dc8f1a26bd93a10dec42ad8ac465fd328c8fbaf0
jonah sobol <jonah.sobol@gmail.com>
2016-02-25 16:44:14 -0800

    test4

--- config/application.rb
+++ config/application.rb
@@ -31,7 +31,8 @@ module Project2App
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
-    config.assets.precompile << /\.(?:svg|eot|woff|ttf)$/
 
   end
+      config.assets.precompile << /\.(?:svg|eot|woff|ttf)$/
+
 end
